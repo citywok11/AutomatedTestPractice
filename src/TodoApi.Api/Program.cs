2856ee9b966b00f063ec5d28c6574ce491f1a7ef
Behnam Moein <54431086+citywok11@users.noreply.github.com>
2025-08-26 17:19:38 +0000

    Added more database stuff

--- src/TodoApi.Api/Program.cs
+++ src/TodoApi.Api/Program.cs
@@ -8,6 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Configure Entity Framework
 builder.Services.AddDbContext<TodoDbContext>(options =>
     options.UseInMemoryDatabase("TodoDb"));
@@ -33,6 +44,13 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
     c.RoutePrefix = "swagger"; // Set Swagger UI at /swagger
 });
+
+// Enable static files and default files
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Enable CORS
+app.UseCors("AllowAll");
 
 // Comment out HTTPS redirection for development
 // app.UseHttpsRedirection();
